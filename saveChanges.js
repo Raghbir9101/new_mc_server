b5d5196c0cb7524f90344a1bb6896379f51bad40
Raghbir9101 <123760432+Raghbir9101@users.noreply.github.com>
2025-02-27 14:22:33 +0000

    2025-02-27T14:22:33.166Z

--- saveChanges.js
+++ saveChanges.js
@@ -26,7 +26,7 @@ function runGitCommands() {
                     console.error(`Error Saving Files: ${stderr} ${stdout}`);
                     return;
                 }
-                console.log(`Game Saved Successfully at ${new Date().toLocaleDateString("in")} ${new Date().toLocaleTimeString("in")} `);
+                console.log(`Game Saved Successfully at ${new Date().toLocaleDateString("in")} ${new Date().toLocaleTimeString("in", {})} `);
             });
         });
     });
