7f434f60f326a69bfe3258d98074d615dfd364fe
raghbir <123760432+Raghbir9101@users.noreply.github.com>
2025-03-01 17:14:58 +0000

    2025-03-01T17:14:58.961Z

--- startSaveChanges.js
+++ startSaveChanges.js
@@ -1,13 +1,13 @@
 const { exec } = require("child_process");
 
 function saveChanges() {
-    exec("node ./saveChanges.js", (err, stdout, stderr) => {
-        if (err) {
-            console.error(`Error adding files: ${stderr}`);
-            return;
-        }
-        console.log("Running saveChanges.js");
-    });
+  exec("node ./saveChanges.js", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error adding files: ${stderr}`);
+      return;
+    }
+    console.log("Saving Game Files", stdout);
+  });
 }
 
 const SAVE_FREQUENCY = 60; // IN SECs
@@ -19,9 +19,9 @@ setInterval(saveChanges, SAVE_FREQUENCY * 1000);
 saveChanges();
 
 exec("java -Xmx10G -Xms10G -jar server.jar nogui", (err, stdout, stderr) => {
-    if (err) {
-        console.error(`Error adding files: ${stderr}`);
-        return;
-    }
-    console.log("Starting Minecraft server", stdout);
-})
+  if (err) {
+    console.error(`Error adding files: ${stderr}`);
+    return;
+  }
+  console.log("Starting Minecraft server", stdout);
+});
