fe2f1d009484cc91bf6100c2b83cfaa7fba8188f
raghbir <123760432+Raghbir9101@users.noreply.github.com>
2025-03-01 17:08:21 +0000

    2025-03-01T17:08:20.254Z

--- saveChanges.js
+++ saveChanges.js
@@ -1,37 +1,43 @@
 const { exec } = require("child_process");
 
 function getCurrentTimestamp() {
-    return new Date().toISOString();
+  return new Date().toISOString();
 }
 
 function runGitCommands() {
-    const commitMessage = `"${getCurrentTimestamp()}"`;
+  const commitMessage = `"${getCurrentTimestamp()}"`;
 
-    exec("git add .", (err, stdout, stderr) => {
+  exec("git add .", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error adding files: ${stderr} ${stdout} ${err}`);
+      return;
+    }
+    console.log("Files added successfully.");
+
+    exec(`git commit -m ${commitMessage}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error committing files: ${stderr} ${stdout} ${err} `);
+        return;
+      }
+      console.log("Files committed successfully.");
+
+      exec("git push --force", (err, stdout, stderr) => {
         if (err) {
-            console.error(`Error adding files: ${stderr} ${stdout} ${err}`);
-            return;
+          console.error(`Error Saving Files: ${stderr} ${stdout} ${err}`);
+          return;
         }
-        console.log("Files added successfully.");
-
-        exec(`git commit -m ${commitMessage}`, (err, stdout, stderr) => {
-            if (err) {
-                console.error(`Error committing files: ${stderr} ${stdout} ${err} `);
-                return;
-            }
-            console.log("Files committed successfully.");
-
-            exec("git push --force", (err, stdout, stderr) => {
-                if (err) {
-                    console.error(`Error Saving Files: ${stderr} ${stdout} ${err}`);
-                    return;
-                }
-                console.log(err, stdout, stderr);
-                console.log(`Game Saved Successfully at ${new Date().toLocaleDateString("in")} ${new Date().toLocaleTimeString("in")} `);
-            });
-        });
+        console.log(err, stdout, stderr);
+        console.log(
+          `Game Saved Successfully at ${new Date().toLocaleDateString(
+            "in"
+          )} ${new Date().toLocaleTimeString("in")} `
+        );
+      });
     });
+  });
 }
 
-
-runGitCommands()
+setTimeout(() => {
+  runGitCommands();
+}, 30000);
+runGitCommands();
